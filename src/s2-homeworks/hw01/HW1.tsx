15854677737b0cc133f636217784e8b149494b15
Ilya_Karachun <Karachun_ilya94@mail.ru>
2022-11-19 16:35:16 +0300

    refactor: clear

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -6,15 +6,6 @@ import FriendMessage from './friend-message/FriendMessage'
 import avatarFriend from './avatar.jpeg'
 import avatarMy from './withCup.jpg'
 
-/*
-* 1 - описать тип MessageType +
-* 2 - описать тип MessagePropsType в файле Message.tsx +
-* 3 - в файле Message.tsx отобразить приходящие данные
-* 4 - выполнить пункты 2, 3 в файле FriendMessage.tsx
-* 5 - сделать стили в соответствии с дизайном
-* */
-
-// нужно создать правильный тип вместо any
 export type MessageType = {
     id: number
     user: {
@@ -55,13 +46,10 @@ const HW1 = () => {
         <div id={'hw1'}>
             <div className={s2.hwTitle}>Chat</div>
             <div className={s2.hw}>
-                {/*проверка отображения (не менять)*/}
                 <div>
                     <Message message={message0} />
                     <FriendMessage message={friendMessage0} />
                 </div>
-
-                {/*для автоматической проверки дз (не менять)*/}
                 <MessageSender M={Message} />
             </div>
         </div>
